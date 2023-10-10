b2bebd60b6982900a358354cdb2a95397c3ea42e
Tonba03 <tonnbph26190@fpt.edu.vn>
2023-10-10 10:56:14 +0700

    Lại thay đổi thêm một chút ColorService

--- shop.Application/ViewModels/Colors/ColorDto.cs
+++ shop.Application/ViewModels/Colors/ColorDto.cs
@@ -4,4 +4,7 @@ public class ColorDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime LastModiFileDate { get; set; }
+
 }
