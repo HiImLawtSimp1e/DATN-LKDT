a9ffd916a5dd0ac4655e51fa8a4c3e1303908e60
vuh111 <vuh1119@gmail.com>
2024-06-28 08:42:54 +0700

    them service cho virtualitem relationobj

--- shop.Domain/Entities/VirtualItemObjRelationEntity.cs
+++ shop.Domain/Entities/VirtualItemObjRelationEntity.cs
@@ -1,68 +1,67 @@
 ﻿using Newtonsoft.Json;
 using shop.Domain.Entities.Base;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace shop.Domain.Entities
+namespace shop.Domain.Entities;
+
+public class VirtualItemObjRelationEntity :BaseEntity
 {
-    public class VirtualItemObjRelationEntity
+    [Key]
+    public Guid Id { get; set; }
+    public Guid? VirtualItemId { get; set; }
+    public Guid? ObjectId { get; set; }
+    public int? Order { get; set; }
+    [NotMapped]
+    public virtual RelatedObjEntity RelatedObj { get; set; }
+    public virtual string RelatedObjJson
     {
-        public Guid Id { get; set; }
-        public Guid? VirtualItem { get; set; }
-        public Guid? ObjectId { get; set; }
-        public int? Order { get; set; }
-        [NotMapped]
-        public virtual RelatedObjEntity RelatedObj { get; set; }
-
-        public virtual string RelatedObjJson
+        get
         {
-            get
+            return RelatedObj == null ? null : JsonConvert.SerializeObject(RelatedObj);
+        }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                RelatedObj = null;
+            else
             {
-                return RelatedObj == null ? null : JsonConvert.SerializeObject(RelatedObj);
-            }
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
+                try
+                {
+                    RelatedObj = JsonConvert.DeserializeObject<RelatedObjEntity>(value);
+                }
+                catch
+                {
                     RelatedObj = null;
-                else
-                {
-                    try
-                    {
-                        RelatedObj = JsonConvert.DeserializeObject<RelatedObjEntity>(value);
-                    }
-                    catch
-                    {
-                        RelatedObj = null;
-                    }
                 }
             }
         }
-
-        [NotMapped]
-        public virtual RelatedObjEntity RelatedObj2 { get; set; }
-
-        public virtual string RelatedObj2Json
-        {
-            get
-            {
-                return RelatedObj2 == null ? null : JsonConvert.SerializeObject(RelatedObj2);
-            }
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                    RelatedObj2 = null;
-                else
-                {
-                    try
-                    {
-                        RelatedObj2 = JsonConvert.DeserializeObject<RelatedObjEntity>(value);
-                    }
-                    catch
-                    {
-                        RelatedObj2 = null;
-                    }
-                }
-            }
-        }
-
     }
+    [NotMapped]
+    public virtual RelatedObjEntity RelatedObj2 { get; set; }
+    public virtual string RelatedObj2Json
+    {
+        get
+        {
+            return RelatedObj2 == null ? null : JsonConvert.SerializeObject(RelatedObj2);
+        }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                RelatedObj2 = null;
+            else
+            {
+                try
+                {
+                    RelatedObj2 = JsonConvert.DeserializeObject<RelatedObjEntity>(value);
+                }
+                catch
+                {
+                    RelatedObj2 = null;
+                }
+            }
+        }
+    }
+    public string? RelationType { get;set; }
+    public bool? Isdeleted { get; set; } =false;
 }
---
+++ shop.Infrastructure/Business/VirtualItemObjRelationEntity/VirtualItemBusiness.cs
@@ -0,0 +1,51 @@
+﻿using shop.Domain.Entities;
+using shop.Infrastructure.Model;
+using shop.Infrastructure.Model.Common.Pagination;
+using shop.Infrastructure.Repositories;
+
+namespace shop.Infrastructure.Business
+{
+    public class VirtualItemBusiness : IVirtualItemBusiness
+    {
+        private readonly IVirtualItemObjRelationRepository _virtualItemObjRelationRepository;
+        public async Task<VirtualItemObjRelationEntity> DeleteAsync(Guid Id)
+        {
+            return await _virtualItemObjRelationRepository.DeleteAsync(Id);
+        }
+
+        public async Task<List<VirtualItemObjRelationEntity>> DeleteAsync(List<Guid> Ids)
+        {
+            return await _virtualItemObjRelationRepository.DeleteAsync(Ids);
+        }
+
+        public async Task<VirtualItemObjRelationEntity> FindAsync(Guid Id)
+        {
+            return await _virtualItemObjRelationRepository.FindAsync(Id);
+        }
+
+        public async Task<List<VirtualItemObjRelationEntity>> FindByVirtualItemAsync(Guid VirtualItemId)
+        {
+            return await _virtualItemObjRelationRepository.FindByVirtualItemAsync(VirtualItemId);
+        }
+
+        public async Task<Pagination<VirtualItemObjRelationEntity>> GetAllAsync(VirtualItemObjRelationQueryModel virtualItemQueryModel)
+        {
+            return await _virtualItemObjRelationRepository.GetAllAsync(virtualItemQueryModel);
+        }
+
+        public async Task<List<VirtualItemObjRelationEntity>> ListAllAsync(VirtualItemObjRelationQueryModel virtualItemQueryModel)
+        {
+            return await _virtualItemObjRelationRepository.ListAllAsync(virtualItemQueryModel);
+        }
+
+        public async Task<VirtualItemObjRelationEntity> SaveAsync(VirtualItemObjRelationEntity virtualItemObjRelationEntity)
+        {
+            return await _virtualItemObjRelationRepository.SaveAsync(virtualItemObjRelationEntity);
+        }
+
+        public async Task<List<VirtualItemObjRelationEntity>> SaveAsync(List<VirtualItemObjRelationEntity> virtualItemObjRelationEntities)
+        {
+            return await _virtualItemObjRelationRepository.SaveAsync(virtualItemObjRelationEntities);
+        }
+    }
+}
