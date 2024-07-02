4a4e690a5d7f78a358c2eada25679de7de9a6430
NXHoangVu <vunxhph19692@fpt.edu.vn>
2024-07-02 23:39:02 +0700

    repo/business Contact+Discount

---
+++ shop.Infrastructure/Business/Discount/IDiscountBusiness.cs
@@ -0,0 +1,22 @@
+﻿using shop.Domain.Entities;
+using shop.Infrastructure.Model.Common.Pagination;
+using shop.Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace shop.Infrastructure.Business.Discount
+{
+    public interface IDiscountBusiness
+    {
+        /*public Task<Pagination<VirtualItemEntity>> GetAllAsync(VirtualItemQueryModel virtualItemQueryModel);
+        public Task<List<VirtualItemEntity>> ListAllAsync(VirtualItemQueryModel virtualItemQueryModel);*/
+        public Task<DiscountEntity> SaveAsync(DiscountEntity discountEntity);
+        public Task<DiscountEntity> PatchAsync(DiscountEntity discountEntity);
+        public Task<List<DiscountEntity>> SaveAsync(List<DiscountEntity> discountEntity);
+        public Task<DiscountEntity> FindAsync(Guid Id);
+        public Task<DiscountEntity> DeleteAsync(Guid Id);
+        public Task<List<DiscountEntity>> DeleteAsync(List<Guid> Id);
+    }
+}
---
+++ shop.Infrastructure/Repositories/Rank/IRankRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using shop.Domain.Entities;
+using shop.Infrastructure.Model;
+using shop.Infrastructure.Model.Common.Pagination;
+
+namespace shop.Infrastructure.Repositories.Rank
+{
+    public interface IRankRepository
+    {
+        /*public Task<Pagination<RankEntity>> GetAllAsync(VirtualItemQueryModel virtualItemQueryModel);
+        public Task<List<RankEntity>> ListAllAsync(VirtualItemQueryModel virtualItemQueryModel);*/
+        public Task<RankEntity> SaveAsync(RankEntity rankEntities);
+        public Task<List<RankEntity>> SaveAsync(List<RankEntity> rankEntities);
+        public Task<RankEntity> FindAsync(Guid Id);
+        public Task<RankEntity> DeleteAsync(Guid Id);
+        public Task<List<RankEntity>> DeleteAsync(List<Guid> Id);
+    }
+}
