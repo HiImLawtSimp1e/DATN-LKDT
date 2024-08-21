b8450dd9fe7bd39581da021799a48cb750f18bcb
hungnqph19112 <nqh1703k2@gmail.com>
2024-08-21 16:53:51 +0700

    FE fixing TagField UI

--- DATN_FE/src/components/ui/tag.tsx
+++ DATN_FE/src/components/ui/tag.tsx
@@ -5,7 +5,9 @@ interface TagFiledProps {
 
 const TagFiled: React.FC<TagFiledProps> = ({ cssClass, context }) => {
   return (
-    <span className={`${cssClass} opacity-75 px-2 py-1 rounded`}>
+    <span
+      className={`${cssClass} opacity-75 px-2 py-1 rounded whitespace-nowrap overflow-hidden text-ellipsis`}
+    >
       {context}
     </span>
   );
