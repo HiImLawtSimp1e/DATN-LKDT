522c3367c95fded87ff9d2460586b565a1af8ac7
hungnqph19112 <nqh1703k2@gmail.com>
2024-09-14 03:08:07 +0700

    nhúng powerBi

---
+++ DATN_FE/src/app/(admin)/dashboard/analytics/page.tsx
@@ -0,0 +1,7 @@
+import PowerBIEmbed from "@/components/dashboard/analytics/powerBIEmbed";
+
+const AnalyticsPage = () => {
+  return <PowerBIEmbed />;
+};
+
+export default AnalyticsPage;
---
+++ DATN_FE/src/components/dashboard/analytics/powerBIEmbed.tsx
@@ -0,0 +1,15 @@
+// components/PowerBIEmbed.js
+const PowerBIEmbed = () => {
+  return (
+    <iframe
+      title="HW12 - Nguyễn Thành Minh"
+      width="1140"
+      height="541.25"
+      src="https://app.powerbi.com/reportEmbed?reportId=63449c52-8061-4156-a453-3146538997b1&autoAuth=true&ctid=447080b4-b9c6-4b0b-92fd-b543a68b4e97"
+      frameBorder="0"
+      allowFullScreen
+    ></iframe>
+  );
+};
+
+export default PowerBIEmbed;
