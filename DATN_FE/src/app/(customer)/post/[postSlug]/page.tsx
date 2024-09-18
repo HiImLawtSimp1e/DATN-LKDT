763ecfdad17250fe9f18dafbd082a4d9a1319805
hungnqph19112 <nqh1703k2@gmail.com>
2024-09-19 00:16:20 +0700

    FE update SEO metadata cho product, blog

--- DATN_FE/src/app/(customer)/post/[postSlug]/page.tsx
+++ DATN_FE/src/app/(customer)/post/[postSlug]/page.tsx
@@ -1,5 +1,6 @@
 import Loading from "@/components/shop/loading";
 import ShopPostDetail from "@/components/shop/post-detail/post-detail";
+import { Metadata } from "next";
 import { Suspense } from "react";
 
 const Post = async ({ postSlug }: { postSlug: string }) => {
@@ -13,6 +14,28 @@ const Post = async ({ postSlug }: { postSlug: string }) => {
 
   return <ShopPostDetail post={post.data} />;
 };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { postSlug: string };
+}): Promise<Metadata> {
+  const { postSlug } = params; // Lấy postSlug từ params
+  const res = await fetch(`http://localhost:5000/api/Blog/${postSlug}`, {
+    method: "GET",
+    next: { tags: ["shopPostDetail"] },
+  });
+
+  const post: ApiResponse<IPost> = await res.json();
+
+  return {
+    title: post?.data?.seoTitle || "FStore - Linh kiện điện tử chính hãng",
+    description:
+      post?.data?.seoDescription || "FStore - Linh kiện điện tử chính hãng",
+    keywords:
+      post?.data?.seoKeyworks || "FStore - Linh kiện điện tử chính hãng",
+  };
+}
 
 const PostDetailPage = ({ params }: { params: { postSlug: string } }) => {
   const { postSlug } = params;
--- DATN_FE/src/app/(customer)/product/[productSlug]/page.tsx
+++ DATN_FE/src/app/(customer)/product/[productSlug]/page.tsx
@@ -1,5 +1,6 @@
 import Loading from "@/components/shop/loading";
 import ShopProductDetail from "@/components/shop/product-detail/product-detail";
+import { Metadata } from "next";
 import { Suspense } from "react";
 
 const Product = async ({ productSlug }: { productSlug: string }) => {
@@ -13,6 +14,30 @@ const Product = async ({ productSlug }: { productSlug: string }) => {
 
   return <ShopProductDetail product={product.data} />;
 };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { productSlug: string };
+}): Promise<Metadata> {
+  const res = await fetch(
+    `http://localhost:5000/api/Product/${params.productSlug}`,
+    {
+      method: "GET",
+      next: { tags: ["shopProductDetail"] },
+    }
+  );
+
+  const product: ApiResponse<IProduct> = await res.json();
+
+  return {
+    title: product?.data?.seoTitle || "FStore - Linh kiện điện tử chính hãng",
+    description:
+      product?.data?.seoDescription || "FStore - Linh kiện điện tử chính hãng",
+    keywords:
+      product?.data?.seoKeyworks || "FStore - Linh kiện điện tử chính hãng",
+  };
+}
 
 const ProductDetailPage = ({ params }: { params: { productSlug: string } }) => {
   const { productSlug } = params;
