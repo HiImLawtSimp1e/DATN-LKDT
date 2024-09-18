afe28b389db8d4b7f2dd1c70663bb731d4341567
hungnqph19112 <nqh1703k2@gmail.com>
2024-09-19 00:15:50 +0700

    FE update analystics

--- DATN_FE/src/components/dashboard/analytics/powerBIEmbed.tsx
+++ DATN_FE/src/components/dashboard/analytics/powerBIEmbed.tsx
@@ -1,14 +1,14 @@
-// components/PowerBIEmbed.js
+// components/PowerBIEmbed.tsx
 const PowerBIEmbed = () => {
   return (
-    <iframe
-      title="HW12 - Nguyễn Thành Minh"
-      width="1140"
-      height="541.25"
-      src="https://app.powerbi.com/reportEmbed?reportId=63449c52-8061-4156-a453-3146538997b1&autoAuth=true&ctid=447080b4-b9c6-4b0b-92fd-b543a68b4e97"
-      frameBorder="0"
-      allowFullScreen
-    ></iframe>
+    <div className="flex justify-center items-center w-full h-screen">
+      <iframe
+        title="DATN"
+        src="https://app.powerbi.com/reportEmbed?reportId=953347c5-62c1-4794-9636-22ee691d9f69&autoAuth=true&ctid=447080b4-b9c6-4b0b-92fd-b543a68b4e97"
+        allowFullScreen={true}
+        className="w-full h-full border-0"
+      ></iframe>
+    </div>
   );
 };
 
